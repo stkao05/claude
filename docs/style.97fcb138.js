940d030220018643d7a13eca2a73a4e14cfd76ca
Steven Kao <st.kao.05@gmail.com>
2020-06-07 23:29:29 +0800

    Book section styling

--- docs/style.97fcb138.js
+++ docs/style.97fcb138.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./claude_gx.ttf":[["claude_gx.a56067a8.ttf","claude_gx.ttf"],"claude_gx.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./claude.ttf":[["claude.5ad181b4.ttf","claude.ttf"],"claude.ttf"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53738" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65274" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
